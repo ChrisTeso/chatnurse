f4c4f43a5a5d108435d0ed714444b218675bf5ff
Chris Teso <Chris.Teso@medicalsolutions.com>
2023-01-12 14:39:59 -0800

    fe

--- hello/views.py
+++ hello/views.py
@@ -146,7 +146,7 @@ def answer_query_with_context(
     return response["choices"][0]["text"].strip(" \n"), context
 
 def index(request):
-    return render(request, "index.html", { "default_question": "How do I become a Traveler with Medical Solutions?" })
+    return render(request, "index.html", { "default_question": "" })
 
 @csrf_exempt
 def ask(request):
